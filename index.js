229e9cb0c4c5dd7680bcf9e7b3108584efa669a9
Jul Jen <juliajen.prog@gmail.com>
2018-10-14 02:10:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+// link is firing function when user clicks 'Get Repositories' anchor tag
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
